23773d26d94f9f65165c7b8f7317e4bc1c47ddf7
BadLuckZ <thanagorn8802@gmail.com>
2026-09-11 22:49:00 +0700

    fix: graceful shutdown again

--- main.go
+++ main.go
@@ -46,7 +46,7 @@ func main() {
 	uc := usecases.NewSubscription(repo)
 	subCtrl := controllers.NewSubscriptionController(uc)
 
-	gs, lis, err := newGRPCServer(cfg.GRPCPort, uc)
+	gs, lis, err := newGRPCServer(ctx, cfg.GRPCPort, uc)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -80,8 +80,13 @@ func main() {
 	gs.GracefulStop()
 }
 
-func newGRPCServer(port string, uc *usecases.SubscriptionUsecase) (*grpclib.Server, net.Listener, error) {
-	lis, err := net.Listen("tcp", ":"+port)
+func newGRPCServer(
+	ctx context.Context,
+	port string,
+	uc *usecases.SubscriptionUsecase,
+) (*grpclib.Server, net.Listener, error) {
+	var lc net.ListenConfig
+	lis, err := lc.Listen(ctx, "tcp", ":"+port)
 	if err != nil {
 		return nil, nil, err
 	}
